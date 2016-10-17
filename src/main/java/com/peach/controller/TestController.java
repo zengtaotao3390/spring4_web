75c22e196abb0bbe76e20073b7baf9e2a4ee8350
zengtaotao3390 <378041351@qq.com>
2016-10-17 16:42:09 +0800

    增加初级版的spring session

--- src/main/java/com/peach/controller/TestController.java
+++ src/main/java/com/peach/controller/TestController.java
@@ -2,14 +2,23 @@ package com.peach.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by zengtao on 2016/10/17.
  */
+@RestController
 public class TestController {
 
-    @RequestMapping(value = "peach", method = RequestMethod.POST)
-    public String peach() throws Exception {
+
+    /**
+     * 使用总结，必须要在这个rest接口中加入HttpSession那么才可以实现这个session共享
+     *
+     */
+    @RequestMapping(value = "peach", method = RequestMethod.GET)
+    public String peach(HttpSession session) throws Exception {
         return "123";
     }
 
